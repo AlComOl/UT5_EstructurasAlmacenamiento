4ad6d9453873fdeb508a44f39bc82cfefc8c415f
AlComOl <alvcomoli@alu.edu.gva.es>
2024-02-01 21:35:55 +0100

    actualiza

--- src/EjerciciosGitHub/Prg5_10.java
+++ src/EjerciciosGitHub/Prg5_10.java
@@ -12,8 +12,10 @@ import java.util.*;
 	public class Prg5_10 {
 		public static void main (String[] args) {
 			Scanner sc=new Scanner(System.in);
+			float media = 0;
+			int suma=0;
 			int aux;//crear un vector con 10 posiciones
-			int v[]= {1,1,1,1,1,1,1,2,2,2};
+			int v[]= {1,1,1,1,1,1,1,1,1,2};
 			int f[]=new int [10];
 				aux=v[v.length-1];
 				System.out.println(aux);
@@ -27,67 +29,36 @@ import java.util.*;
 				for(int i=0;i<v.length;i++) {
 				System.out.println(v[i]);	
 				}
-				
+//				******************************************************************/
 			for(int i=0;i<v.length;i++) {
 				int temp=v[i];
 			
-				switch (temp) {
-				case 0: 
-					f[i++]=temp;
-					
-					break;	
-				case 1:
-					f[i]=temp;
-					
-					break;
-				case 2:
-					f[i]=temp;
-					
-					break;
-				case 3:
-					f[i]=temp;
-				
-					break;
-				case 4:
-					f[i]=temp;
-				
-					break;
-				case 5:
-					f[i]=temp;
-				
-					break;
-				case 6:
-					f[i]=temp;
-					
-					break;
-				case 7:
-					f[i]=temp;
-				
-					break;
-				case 8:
-					f[i]=temp;
-				
-					break;
-				case 9:
-					f[i]=temp;
-				
-					break;
-				case 10:
-					f[i]=temp;
-				
-					break;
-				
-				default:
-					break;
-				}	
 			}
 			
 			System.out.println("***************VECTOR CON FRECUENCIAS***************");
-		
+	
+			for( int i=0;i<v.length;i++) {
+				
+				int valor=v[i];
+				
+						f[valor]++;
+						
+								
+						
+			}
+			System.out.println("***************VECTOR CON FRECUENCIAS***************");	
+			for( int i=0;i<v.length;i++) {
+				f[i]=f[i]*i;
+				System.out.println(f[i]);
+			}
 			
-			
-			
+			for(int i=0;i<f.length;i++) {
+				suma+=f[i];
+			}
+//		voy por calcular la moda
+			System.out.println("La media aritmetica de los elementos del vector es "+(media=(float)suma/10));
+//			System.out.println("La moda de los elementos del del vector es :");
 		}
+	
 	}
-
 
