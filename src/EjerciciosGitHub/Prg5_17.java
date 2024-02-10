0d1fb05370008ea652d7869e544e9efdfa9fa890
AlComOl <alvcomoli@alu.edu.gva.es>
2024-02-10 11:06:51 +0100

    actualiza

--- src/EjerciciosGitHub/Prg5_17.java
+++ src/EjerciciosGitHub/Prg5_17.java
@@ -19,11 +19,16 @@ public class Prg5_17 {
 		int m[][]=new int[N][N];
 		
 		cargarMatriz(m,sc);
-		mostrarMatriz(m);
+		
 		
 //		diagonalPrincipal(m,rdo);
 		
-		diagonalSecundaria(m,rdo);
+//		diagonalSecundaria(m,rdo);
+//		compruebaMartiz(m);
+		if(compruebaMartiz(m)==true) {
+		diagonalPrincipal(m,rdo);
+		}
+		mostrarMatriz(m);
 		
 		for(int i=0;i<rdo.length;i++) {
 			System.out.println(rdo[i]);
@@ -34,47 +39,57 @@ public class Prg5_17 {
 
 	}
 	
-	
-	public static boolean diagonalPrincipal(int m[][],int rdo[]) {
+	public static void diagonalPrincipal(int m[][],int rdo[]) {
+		/****************************************************
+		 * @author acome
+		 *  @param matriz
+		 * @param vector con el resultado
+		 * Metodo que suma la diagonal Principal con 1 solo bucle
+		 *****************************************************/
 		
-		boolean aceptada=false;
 		int suma=0;
-		if(m.length==m[0].length) {
-			aceptada=true;
+			for(int i=0;i<m.length;i++) {
+					suma+=m[i][i];	
+			}
+				rdo[0]=suma;
+	
+}
+	public static void diagonalPrincipal1(int m[][],int rdo[]) {
+		/****************************************************
+		 * @author acome
+		 *  @param matriz
+		 * @param vector con el resultado
+		 * Metodo que suma la diagonal Principal con 2 bucles
+		 *****************************************************/
+		
+		int suma=0;
+		
 			for(int i=0;i<m.length;i++) {
 				for(int j=i;j<i+1;j++) {
 					suma+=m[i][j];
-					
+			}
 				}
 				
-			}
-			rdo[0]=suma;
-		}else {
-			System.out.println("la matriz no es cuadrada");
-		}
-		return aceptada;	
+			
+			rdo[0]=suma;	
 	}
 	
 	
-public static boolean diagonalSecundaria(int m[][],int rdo[]) {
-		
-		boolean aceptada=false;
-		int suma=0;
-		if(m.length==m[0].length) {
-			aceptada=true;
-			for(int i=m.length;i>m.length-1;i--) {
-				for(int j=i;j<i-1;j--) {
-					suma+=m[i][j];
-					
-				}
-				
-			}
-			rdo[0]=suma;
-		}else {
-			System.out.println("la matriz no es cuadrada");
+	public static void  diagonalSecundaria(int m[][],int rdo[]) {
+	/***********************************************************
+	 * @author acome
+	 * @param matriz
+	 * @param vector con el resultado
+	 * Metodo que suma la diagonal imversa con 1 solo bucle
+	 * *********************************************************/
+			
+			int suma=0;
+			
+			for(int i=0;i<=m.length-1;i++) {//filas y columnas
+				suma+=m[i][2-i];
+			}	
+			rdo[0]=suma;	
 		}
-		return aceptada;	
-	}
 	
 	
 
@@ -88,6 +103,18 @@ public static boolean diagonalSecundaria(int m[][],int rdo[]) {
 			}
 		}
 	
+	}
+	
+	public static boolean compruebaMartiz(int m[][]) {
+		boolean aceptada=false;
+	
+		
+		if(m.length==m[0].length) {
+			aceptada=true;
+		}else {
+			System.out.println("la matriz no es cuadrada");
+		}
+		return aceptada;
 	}
 	
 	
@@ -105,3 +132,5 @@ public static boolean diagonalSecundaria(int m[][],int rdo[]) {
 		
 	}
 }
+	
+	
